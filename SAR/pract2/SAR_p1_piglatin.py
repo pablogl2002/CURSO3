d3db391c0a35d30f6fc3616ddfbfbc3833b6d845
Pablo García <pablogl2002@gmail.com>
2023-02-26 13:47:47 +0100

    SAR practica 1 Terminada

--- SAR/pract2/SAR_p1_piglatin.py
+++ SAR/pract2/SAR_p1_piglatin.py
@@ -20,7 +20,7 @@ class Translator():
         :return: el objeto de tipo Translator
         """
         if punt is None:
-            punt = ".,;?!"
+            punt = ".,;?'!"
         self.re = re.compile(r"(\w+)([" + punt + r"]*)")
 
     def translate_word(self, word:Text):
@@ -30,51 +30,62 @@ class Translator():
         :param word: la palabra que se debe pasar a Pig Latin
         :return: la palabra traducida
         """
-        punt = ".,;?!"
-        vocales = 'aeiouAEIOU'
-        ay = "ay"
-        yay = "yay"
+        vocales = 'aeiouyAEIOUY'    # definicion de una variable que contenga todas las vocales
+        ay = "ay"                   # definicion de una variable ay que añadiremos al final de las palabras (puede cambiar entre minusculas y mayusculas)        
+        yay = "yay"                 # definicion de una variable yay que añadiremos al final de las palabras que empiezen por vocal
+                                        # (puede cambiar entre minusculas y mayusculas)
 
+
+        # comprueba si el primer simbolo no es un letra
         if word[0] not in string.ascii_letters:
+            # si no es una letra deja la palabra igual
             new_word = word
         else:
+            # comprueba si la palabra está escrita en mayúsculas
             if word.upper() == word:
+                # si mayusculuas -> cambia los sufijos a mayusculas
                 ay = "AY"
                 yay = "YAY"
-            capL = False
-            for i in range(len(word)):
-                hasV = False
-
-                if word[0] in string.ascii_uppercase and word[1:].lower() == word[1:]:
-                    word = word.lower()
-                    capL = True
-
-                if word[0] in vocales:
-                    if word[-1] in punt:
-                    #if self.re.search(word):
-                        new_word = word[:-1] + yay + word[-1]
-                    else: new_word = word + yay
-                    break
-                else:
-                    if word[i] in vocales:
-                        if word[-1] in punt:
-    #                    if self.re.search(word):
-                            new_word = word[i:-1] + word[:i] + ay + word[-1]
-                        else:
-                            new_word = word[i:] + word[:i] + ay
-                        hasV = True
-                        break
-
-                    if hasV == False and i == len(word) - 1:
-                        if word[-1] in punt:
-#                        if self.re.search(word):
-                            new_word = word[:-1] + ay + word[-1]
-                        else: new_word = word + ay
-
-            if capL:
-                new_word = new_word[0].upper() + new_word[1:]
-            #new_word = word # SUSTITUIR ESTA PARTE
             
+            capL = False # variable que indicará si la primera letra de una palabra está en mayusculas (capital letter)
+            
+            # comprueba si la primera letra está en mayusculas y lo demas en minusculas
+            if word[0] in string.ascii_uppercase and word[1:].lower() == word[1:]:
+                # convierte la palabra a minusculas 
+                word = word.lower()
+                # pone la variable capital letter a true para luego poner la primera letra del resultado en mayuscula
+                capL = True
+
+            # comprueba si la primera letra es vocal
+            if word[0] in vocales:
+                # si es vocal el primer simbolo añade yay al final
+                new_word = word + yay
+            else:
+                # si la primera no es vocal recorre la palabra por posiciones
+                for i in range(len(word)):
+
+                    hasV = False # variable que indicará si la palabra tiene vocales o no
+
+                    # busca vocales en las posiciones 
+                    if word[i] in vocales:
+                        # si encuentra vocal añade a partir de la posicion delante y antes de la posicion detrás + ay
+                        new_word = word[i:] + word[:i] + ay
+                        # pone la variable tiene vocales a True
+                        hasV = True
+                        # corta el bucle
+                        break
+                    
+                    # comprueba que ha recorrido toda la palabra pero no ha encontrado vocales
+                    if hasV == False and i == len(word) - 1:
+                        # añade ay a la palabra original
+                        new_word = word + ay
+
+            # comprueba si la variable capital letter es true
+            if capL:
+                # si es true, pone la primera letra del resultado en mayuscula
+                new_word = new_word[0].upper() + new_word[1:]
+
+        # devuelve la palabra            
         return new_word
 
     def translate_sentence(self, sentence:Text):
@@ -86,17 +97,18 @@ class Translator():
         """
 
         # traducir una frase #
-
-        sentenceL = sentence.split()
         t = Translator()
         new_sentence = ""
-        for word in sentenceL:
-            # añadir palabras en un str
-            #new_sentence.concat(t.translate_word(word) + " ")
-            new_sentence += t.translate_word(word) + " "
 
-        # new_sentence = sentence # SUSTITUIR ESTA PARTE
+        # separa palabras y simbolos en una lista de duplas 
+        tokens = self.re.findall(sentence)
 
+        # recorre la lista de duplas
+        for (word, punt) in tokens:
+            # para cada palabra en la frase llama a que se traduzca y le añade el correspondiente simbolo después
+            new_sentence += t.translate_word(word) + punt + " "
+
+        # devuelve al frase
         return new_sentence
 
     def translate_file(self, filename:Text):
@@ -111,13 +123,21 @@ class Translator():
             print(f'{filename} no existe o no es un nombre de fichero', file=sys.stderr) 
         # COMPLETAR
         else:
+            # abre el fichero con el nombre en consola
             f = open(filename, "r")
-            t = Translator()
-            with open(filename[:-4] + "_piglatin.txt", mode='w') as file_object:
-                for line in f:  
-                    print(t.translate_sentence(line), file=file_object)
-                    #t.translate_sentence(line)
 
+            # crea un traductor
+            t = Translator()
+            # separa el nombre del fichero por el punto de la extension
+            fileN = filename.split(".")
+            # crea un archivo (o "abre" un archivo) con el mismo nombre que el anterior + _latin y lo instancia como file_object
+            with open(fileN[0] + "_latin." + ".".join(fileN[1:]), mode='w') as file_object:
+                # recorre el fichero por lineas/frases
+                for line in f:
+                    # escribe en el nuevo fichero las lineas traducidas
+                    print(t.translate_sentence(line), file=file_object)
+
+            # cierra el primer fichero
             f.close()
 
 if __name__ == "__main__":
@@ -131,5 +151,4 @@ if __name__ == "__main__":
         sentence = input("ENGLISH: ")
         while len(sentence) > 1:
             print("PIG LATIN:", t.translate_sentence(sentence))
-            #print("PIG LATIN:", t.translate_word(sentence))
             sentence = input("ENGLISH: ")
