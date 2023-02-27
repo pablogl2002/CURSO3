af1f453bf65dd5d8f69c063e9166e143b3295471
Pablo García <pablogl2002@gmail.com>
2023-02-27 15:16:20 +0100

    SAR pract1 retoques

--- SAR/pract1/SAR_p1_piglatin.py
+++ SAR/pract1/SAR_p1_piglatin.py
@@ -34,8 +34,6 @@ class Translator():
         ay = "ay"                   # definicion de una variable ay que añadiremos al final de las palabras (puede cambiar entre minusculas y mayusculas)        
         yay = "yay"                 # definicion de una variable yay que añadiremos al final de las palabras que empiezen por vocal
                                         # (puede cambiar entre minusculas y mayusculas)
-
-
         # comprueba si el primer simbolo no es un letra
         if word[0] not in string.ascii_letters:
             # si no es una letra deja la palabra igual
@@ -46,16 +44,13 @@ class Translator():
                 # si mayusculuas -> cambia los sufijos a mayusculas
                 ay = "AY"
                 yay = "YAY"
-            
             capL = False # variable que indicará si la primera letra de una palabra está en mayusculas (capital letter)
-            
             # comprueba si la primera letra está en mayusculas y lo demas en minusculas
             if word[0] in string.ascii_uppercase and word[1:].lower() == word[1:]:
                 # convierte la palabra a minusculas 
                 word = word.lower()
                 # pone la variable capital letter a true para luego poner la primera letra del resultado en mayuscula
                 capL = True
-
             # comprueba si la primera letra es vocal
             if word[0] in vocales:
                 # si es vocal el primer simbolo añade yay al final
@@ -63,9 +58,7 @@ class Translator():
             else:
                 # si la primera no es vocal recorre la palabra por posiciones
                 for i in range(len(word)):
-
                     hasV = False # variable que indicará si la palabra tiene vocales o no
-
                     # busca vocales en las posiciones 
                     if word[i] in vocales:
                         # si encuentra vocal añade a partir de la posicion delante y antes de la posicion detrás + ay
@@ -74,17 +67,14 @@ class Translator():
                         hasV = True
                         # corta el bucle
                         break
-                    
                     # comprueba que ha recorrido toda la palabra pero no ha encontrado vocales
                     if hasV == False and i == len(word) - 1:
                         # añade ay a la palabra original
                         new_word = word + ay
-
             # comprueba si la variable capital letter es true
             if capL:
                 # si es true, pone la primera letra del resultado en mayuscula
-                new_word = new_word[0].upper() + new_word[1:]
-
+                new_word = new_word.capitalize()
         # devuelve la palabra            
         return new_word
 
@@ -95,19 +85,15 @@ class Translator():
         :param sentence: la frase que se debe pasar a Pig Latin
         :return: la frase traducida
         """
-
         # traducir una frase #
         t = Translator()
         new_sentence = ""
-
         # separa palabras y simbolos en una lista de duplas 
         tokens = self.re.findall(sentence)
-
         # recorre la lista de duplas
         for (word, punt) in tokens:
             # para cada palabra en la frase llama a que se traduzca y le añade el correspondiente simbolo después
             new_sentence += t.translate_word(word) + punt + " "
-
         # devuelve al frase
         return new_sentence
 
@@ -118,14 +104,11 @@ class Translator():
         :param filename: el nombre del fichero que se debe traducir
         :return: None
         """
-
         if not isfile(filename):
             print(f'{filename} no existe o no es un nombre de fichero', file=sys.stderr) 
-        # COMPLETAR
         else:
             # abre el fichero con el nombre en consola
             f = open(filename, "r")
-
             # crea un traductor
             t = Translator()
             # separa el nombre del fichero por el punto de la extension
@@ -136,7 +119,6 @@ class Translator():
                 for line in f:
                     # escribe en el nuevo fichero las lineas traducidas
                     print(t.translate_sentence(line), file=file_object)
-
             # cierra el primer fichero
             f.close()
 
