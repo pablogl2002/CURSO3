8c8f2aa13d193ab36aec3274fbeb3aa4ddbe364b
Pablo García <pablogl2002@gmail.com>
2023-03-21 12:07:21 +0100

    SAR pract3 terminada

--- SAR/pract3/SAR_p3_monkey_lib.py
+++ SAR/pract3/SAR_p3_monkey_lib.py
@@ -3,7 +3,7 @@
 # 3.- Mono Library
 
 import pickle
-import random
+import random as rd
 import re
 import sys
 from typing import List, Optional, TextIO
@@ -45,16 +45,14 @@ class Monkey():
         n = self.info['n'] 
 
         cont = 1
-        # recorrermos hasta el numero maximo de ngramas (n) para ir rellanando el diccionario
+        # recorremos hasta el numero maximo de ngramas (n) para ir rellanando el diccionario
         for c in range(2, n+1):
         
             # pasamos la frase de string a un array [$, pal1, pal2, pal3..., $]
             words =  sentence.split()
-            # bucle que recorre el tamaño del ngrama, y poner antes de la frase tantos '$' como toquen (n - 1)
-            for w in range(n - 1): 
-                ### Pablo : deberia ser c en vez de n-1??? en cuyo caso habria q cambiar el bucle de c
+            # bucle que recorre el tamaño del ngrama, y poner antes de la frase tantos '$' como toquen (c - 1)
+            for w in range(c - 1): 
                 words = ['$'] + words # añadimos delante del array un '$' por iteracion
-            print(words)
 
 
             for w in range(len(words) - (c-1)):
@@ -87,7 +85,7 @@ class Monkey():
             self.info['lm'][i] = {}
         for filename in filenames:
             for line in open(filename, encoding='utf-8'):
-                
+
                 #se pasan las lineas del texto quitando los simbolos
                 line = self.r1.split(line.strip())
 
@@ -105,6 +103,7 @@ class Monkey():
 
                     #llamamos a index_sentence para todas las frases completas
                     for f in line[:-1]:
+                        # añadimos ya el simbolo '$' para indicar el final de la frase
                         self.index_sentence(f + ' $')
 
                 #Si el buffer NO está vacío
@@ -112,7 +111,7 @@ class Monkey():
 
                     #Comprobamos que la linea que estamos leyendo no esté vacia para poder eliminar el \n\n
                     if len(line) == 1 and line[0] == '':
-                        #llamamos a index_sentence para la frase
+                        #llamamos a index_sentence para la frase ya con el dolar indicando final de frase
                         self.index_sentence(buffer + ' $')
                         buffer = None #se elimina el buffer
                     
@@ -129,7 +128,12 @@ class Monkey():
 
                         #llamamos a index_sentence para todas las frases completas
                         for f in line[:-1]:
+                            # frase + '$' final de frase
                             self.index_sentence(f + ' $')
+        # en el caso de que se quede en el buffer la ultima frase, por no tener un doble salto de linea o un indicativo de final de linea
+        if buffer != None:
+            # llamamos a index_sentencence de lo q resta de buffer (ultima frase) con el simbolo '$' indicando el final de frase
+            self.index_sentence(buffer + ' $')
                         
         for i in range(2, n+1):
             convert_to_lm_dict(self.info['lm'][i])
@@ -167,17 +171,65 @@ class Monkey():
 
 
     def generate_sentences(self, n:Optional[int], nsentences:int=10, prefix:Optional[str]=None):
-        #############
-        # COMPLETAR #
-        #############
+        
+        pref = ""
+        aux = ""
+        max_palabras = 50
+        
+        #Si no pasan ningun prefijo, completalo con '$'
+        if prefix == None:
+            aux = ['$'] * (n-1)
+
+        #Si nos pasan el prefijo:
+        else:
+            prefix = (re.sub(self.r2,' ',prefix)).lower()
+            prefix = prefix.split()
+            
+            #Comprobamos que el prefijo no sobrepase la n
+            if len(prefix) >= n - 1:
+                aux = prefix[-(n-1):]
+
+            #Si no lo sobrepasa completa con $ el principio del prefijo
+            else:
+                pref = (self.r2.sub(' ',prefix)).lower()
+                for _ in range(n - 1 - len(prefix)):
+                    prefix = ['$'] + prefix
+                aux = prefix
+
+        #prefijo inicial de n-1 palabras que se utiliza para reestablecer el prefijo "aux" en cada frase distinta
+        aux2 = tuple(aux)
+
+        #Para cada frase que vamos a generar
+        for _ in range(nsentences):
+
+            #Primero escribimos el prefijo para cada frase
+            print(pref + " ", end='')
+            #tupla de n-1 palabras de la frase que se van generando
+            aux = aux2
+            
+            #si no supera el limite de palabras
+            for _ in range(max_palabras):
+                #escoge la clave que corresponda al preijo
+                c = self.info['lm'][n][aux]
+                values = c[1] #guarda los valores
+                weights = [v[0] for v in values] #guarda los pesos
+                palabras = [w[1] for w in values] #guarda las palabras
+
+                #para la siguiente palabra haz una elección random
+                next_word = rd.choices(palabras, weights, k = 1)
+                #si la siguiente palabra es un '$' acaba la frase
+                if next_word == ['$']:
+                    break
+                #el prefijo pasa a ser (w1, w2) a (w2, w3) siendo w3 la palabra escogida anteriormente
+                aux = aux[1:] + tuple(next_word)
+                #escribe la palabra por pantalla
+                print(' '.join(next_word) + " ", end='')
+            print()
+            
+    
+            
 
 
-
-
-
-
-
-        pass
 
 
 if __name__ == "__main__":
